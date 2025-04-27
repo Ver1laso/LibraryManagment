5b4633a18cf9ce71119060153969f539ed1d3b5b
Ver1laso <69420275+Ver1laso@users.noreply.github.com>
2025-04-27 11:59:09 +0100

    DTO

--- src/main/java/dev/jlprisan/LibraryManagment/Credentials.java
+++ src/main/java/dev/jlprisan/LibraryManagment/Credentials.java
@@ -1,7 +1,7 @@
 package dev.jlprisan.LibraryManagment;
 
 
-import jakarta.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
